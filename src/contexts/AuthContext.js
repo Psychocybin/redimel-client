445c8b2571377dbf3669a634e9f54eed261966f0
GeorgiGeorgiev <boyanov81gg@gmail.com>
2024-02-09 12:26:48 +0200

    adding automatic logout

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import useLocalStorage from '../hooks/useLocalStorage';
+import useLocalStorage, { getTokenDuration, removeTokenAndTokenDuration, setTokenDuration } from '../hooks/useLocalStorage';
 
 export const AuthContext = createContext();
 
@@ -13,19 +14,34 @@ export function AuthProvider({
 }) {
 	const [user, setUser] = useLocalStorage('user', initialAuthState);
 	const [additionalMessage, setAdditionalMessage] = useState(null);
+    const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!user.jwtToken) {
+			return;
+		}
+
+		const tokenDuration = getTokenDuration();
+
+		setTimeout(() => {
+			navigate('/logout');
+		}, tokenDuration);
+	}, [user.jwtToken, navigate]);
 
 	useEffect(() => {
 		setTimeout(() => {
 			setAdditionalMessage(null);
 		}, 10000);
-	}, [additionalMessage])
+	}, [additionalMessage]);
 
 	function login(authData) {
 		setUser(authData);
+		setTokenDuration();
 	}
 
 	function logout() {
 		setUser(initialAuthState);
+		removeTokenAndTokenDuration();
 	}
 
 	function changeMessage(message) {
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,32 @@
 import { useState } from "react";
 
+export function getTokenDuration() {
+    const storedExpirationDate = localStorage.getItem('expiration');
+    const expirationDate = new Date(storedExpirationDate);
+    const now = new Date();
+    const duration = expirationDate.getTime() - now.getTime();
+
+    return duration;
+}
+
+export function setTokenDuration() {
+    const expiration = new Date();
+    expiration.setHours(expiration.getHours() + 1);
+    localStorage.setItem('expiration', expiration.toISOString());
+}
+
+export function removeTokenAndTokenDuration() {
+    localStorage.removeItem('user');
+	localStorage.removeItem('expiration');
+}
+
 export default function useLocalStorage(key, initialValue) {
     const [state, setState] = useState(() => {
         try {
             let item = localStorage.getItem(key);
 
-            return item 
-                ? JSON.parse(item) 
+            return item
+                ? JSON.parse(item)
                 : initialValue;
         } catch (error) {
             return initialValue;
